4cfae51f662af230a48c48358a0ed5dc45e77127
francislub <lubanjwafrancis@gmail.com>
2024-04-07 10:02:10 +0300

    final product

---
+++ app/src/main/java/com/francis/aboutme2/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.francis.aboutme2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
+    private fun setListeners(){
+        //val clickableViews: List<View> =
+        //    listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text, constraint_layout)
+
+        //for (item in clickableViews){
+        //    item.setOnClickListener{ makeColored(it)}
+        //}
+    }
+    private
+    fun makeColored(view: View){
+        when (view.id){
+            //R.id.box_one_text -> view.setBackgroundColor(color.DKGRAY)
+            //R.id.box_two_text -> view.setBackgroundColor(color.GRAY)
+
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            //else -> view.setBackgroundColor(color.LTGRAY)
+        }
+    }
+}
